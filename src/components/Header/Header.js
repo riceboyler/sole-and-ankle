2abc022f70dc25b58ab59f940901b7f387110d19
Jason Clark <riceboyler@gmail.com>
2021-11-24 07:47:18 -0800

    Header flexed out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,20 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 16px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  gap: 48px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
